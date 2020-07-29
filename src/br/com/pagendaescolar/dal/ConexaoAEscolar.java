09223a2c8870d5d911b04e2c2e7138f426425324
Francisco Souza <fran.sf@uol.com.br>
2020-07-29 18:19:08 -0300

    Primera versão privada, com Adicionar

---
+++ src/br/com/pagendaescolar/dal/ConexaoAEscolar.java
@@ -0,0 +1,62 @@
+package br.com.pagendaescolar.dal;
+
+
+//import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author franz
+ */
+public class ConexaoAEscolar {
+    
+    static Connection conexao = null;
+    static PreparedStatement pst = null;
+    static ResultSet rs = null;
+    
+    //metodo responsavel por estabelecer a conexao com o BD
+    public static Connection conector() {
+
+        Connection conn = null;
+        
+        try {
+            
+            String url = "jdbc:sqlite:/home/franz/sql_Agenda_Escolar";
+            
+            Connection conexao = DriverManager.getConnection(url);
+            
+            System.out.println("Conexão estabelecida!");
+            
+            return conexao;
+            
+            
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+//        }finally {
+//            try {
+//                if (conn != null) {
+//                    conn.close();
+//                }
+//            } catch (SQLException ex) {
+//                System.out.println(ex.getMessage());
+//            }
+//        }
+        
+    }
+
+
+}
